3b855a6cfbeef18136aa6d02bc8ebfebd0a9ef18
renratwit <raymond.ren9@gmail.com>
2021-12-28 20:39:56 -0500

    more tetriminos

--- myproject/main.js
+++ myproject/main.js
@@ -18,30 +18,145 @@ camera.position.setZ(30)
 renderer.render(scene, camera)
 
 //
-const material = new THREE.MeshBasicMaterial({color: 0xCCB511 });
+function createOPiece() {
+  const material = new THREE.MeshBasicMaterial({color: 0xCCB511 });
 
-const group = new THREE.Group()
-const geometry = new THREE.BoxGeometry(10, 10, 10)
+  const group = new THREE.Group()
+  const geometry = new THREE.BoxGeometry(10, 10, 10)
 
-const cubeA = new THREE.Mesh(geometry, material)
-cubeA.position.set(10, 10, 0);
+  const cubeA = new THREE.Mesh(geometry, material)
+  cubeA.position.set(10, 10, 0);
 
-const cubeB = new THREE.Mesh(geometry, material)
-cubeB.position.set(10, 20.15, 0)
+  const cubeB = new THREE.Mesh(geometry, material)
+  cubeB.position.set(10, 20.15, 0)
 
-const cubeC = new THREE.Mesh(geometry, material)
-cubeC.position.set(20.15, 10, 0)
+  const cubeC = new THREE.Mesh(geometry, material)
+  cubeC.position.set(20.15, 10, 0)
 
-const cubeD = new THREE.Mesh(geometry, material)
-cubeD.position.set(20.15, 20.15, 0)
+  const cubeD = new THREE.Mesh(geometry, material)
+  cubeD.position.set(20.15, 20.15, 0)
 
-group.add(cubeA)
-group.add(cubeB)
-group.add(cubeC)
-group.add(cubeD)
+  group.add(cubeA)
+  group.add(cubeB)
+  group.add(cubeC)
+  group.add(cubeD)
 
-group.position.x = 60
-scene.add(group)
+  group.rotation.y = 90
+  group.position.x = 60
+  return group;
+}
+scene.add(createOPiece())
+
+function createTPiece() {
+  const material = new THREE.MeshBasicMaterial({color: 0x7734eb });
+  const group = new THREE.Group()
+  const geometry = new THREE.BoxGeometry(10, 10, 10)
+
+  const cubeA = new THREE.Mesh(geometry, material)
+  cubeA.position.set(10, 10, 0);
+  group.add(cubeA)
+
+  const cubeB = new THREE.Mesh(geometry, material)
+  cubeB.position.set(10, 20.15, 0)
+  group.add(cubeB)
+
+  const cubeC = new THREE.Mesh(geometry, material)
+  cubeC.position.set(10, 20.15, 10)
+  group.add(cubeC)
+
+  const cubeD = new THREE.Mesh(geometry, material)
+  cubeD.position.set(10, 20.15, -10)
+  group.add(cubeD)
+
+  group.position.x = 70
+  group.position.y = 30
+
+  return group;
+}
+scene.add(createTPiece())
+
+function createIPiece() {
+  const material = new THREE.MeshBasicMaterial({color: 0x21cceb });
+  const group = new THREE.Group()
+  const geometry = new THREE.BoxGeometry(10, 10, 10)
+
+  const cubeA = new THREE.Mesh(geometry, material)
+  cubeA.position.set(10, 10, 0);
+  group.add(cubeA)
+
+  const cubeB = new THREE.Mesh(geometry, material)
+  cubeB.position.set(10, 20.15, 0);
+  group.add(cubeB)
+
+  const cubeC = new THREE.Mesh(geometry, material)
+  cubeC.position.set(10, 30.3, 0);
+  group.add(cubeC)
+
+  const cubeD = new THREE.Mesh(geometry, material)
+  cubeD.position.set(10, 40.45, 0);
+  group.add(cubeD)
+
+  group.position.x = 45
+  group.position.y = 45
+
+  return group
+}
+scene.add(createIPiece())
+
+function createLPiece() {
+  const material = new THREE.MeshBasicMaterial({color: 0xdb7b14 });
+  const group = new THREE.Group()
+  const geometry = new THREE.BoxGeometry(10, 10, 10)
+
+  const cubeA = new THREE.Mesh(geometry, material)
+  cubeA.position.set(10, 10, 0);
+  group.add(cubeA)
+
+  const cubeB = new THREE.Mesh(geometry, material)
+  cubeB.position.set(10, 20.15, 0);
+  group.add(cubeB)
+
+  const cubeC = new THREE.Mesh(geometry, material)
+  cubeC.position.set(10, 30.3, 0);
+  group.add(cubeC)
+
+  const cubeD = new THREE.Mesh(geometry, material)
+  cubeD.position.set(10, 10, 10);
+  group.add(cubeD)
+
+  group.position.x = 55
+  group.position.y = 30
+
+  return group;
+}
+scene.add(createLPiece())
+
+function createJPiece() {
+  const material = new THREE.MeshBasicMaterial({color: 0x0335fc });
+  const group = new THREE.Group()
+  const geometry = new THREE.BoxGeometry(10, 10, 10)
+
+  const cubeA = new THREE.Mesh(geometry, material)
+  cubeA.position.set(10, 10, 0);
+  group.add(cubeA)
+
+  const cubeB = new THREE.Mesh(geometry, material)
+  cubeB.position.set(10, 20.15, 0);
+  group.add(cubeB)
+
+  const cubeC = new THREE.Mesh(geometry, material)
+  cubeC.position.set(10, 30.3, 0);
+  group.add(cubeC)
+
+  const cubeD = new THREE.Mesh(geometry, material)
+  cubeD.position.set(10, 10, -10);
+  group.add(cubeD)
+
+  group.position.z = 50
+
+  return group;
+}
+scene.add(createJPiece())
 
 //
 
