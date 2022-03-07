f3664a212128b87ea64c9874cec6ec2a50c1698a
renratwit <raymond.ren9@gmail.com>
2022-03-07 11:07:08 -0500

    clean up

--- myproject/script.js
+++ myproject/script.js
@@ -1,26 +1,6 @@
-// setTimeout(() => {
-
-// }, 5000)
-
-// document.querySelector('#contact').scrollIntoView({
-//     behavior: 'smooth'
-// });
-
 function scrollToView(element) {
     console.log(element)
     document.querySelector(element).scrollIntoView({
         behavior: 'smooth'
     })
 }
-
-//Check if given integer is even
-const isEven = (n) => {
-    let inc = 2;
-    if (n < 0) inc = -inc;
-
-    if (n === 0) return true;
-    if (n === 1 || n === -1) return false;
-    return isEven(n - inc)
-}
-
-console.log(isEven(-10))
